cdf6d6e9715979eb6a7a6f5aca36375d8097f891
EgorSkeleton <113343110+EgorSkeleton@users.noreply.github.com>
2026-01-23 17:25:37 +1000

    Delete Desktop/FoldersPython/Peaceful Mode/PeacefulMode directory

--- Desktop/FoldersPython/Peaceful Mode/PeacefulMode/inp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inp_home, name='inp_home'),
-]
--- Desktop/FoldersPython/Peaceful Mode/PeacefulMode/inp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def inp_home(request):
-    return render(request, 'inp/inp_home.html')
